f701f0b4783ddf6421bd90ae6bd33b60cfc734d5
Hongarc <anhhong12121997t@gmail.com>
2018-06-25 21:27:32 +0700

    Log process.env.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const timer = require('./lib/timer');
 const DEFAULT_PORT = 1997;
 const DEFAULT_IP = '127.0.0.1';
 
+log.info('process.env');
+log.info(process.env);
 const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || DEFAULT_PORT;
 const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || DEFAULT_IP;
 app.listen(port, ip, log.info('This app is running in port', port));
