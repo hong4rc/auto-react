06a4057b068e68700cb14e977492ca993fb86de2
Hongarc <anhhong12121997t@gmail.com>
2018-06-25 20:26:57 +0700

    Simple response.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const DEFAULT_IP = '127.0.0.1';
 const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || DEFAULT_PORT;
 const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || DEFAULT_IP;
 app.listen(port, ip, log.info('This app is running in port', port));
+app.get('/', (req, res)=>{
+    res.send('Running!!!');
+});
 
 api().then(func => {
     func.start();
