bc05cbe75ccab8cf0e6f513db66c25a727884a46
Hongarc <anhhong12121997t@gmail.com>
2018-07-06 03:24:14 +0700

    Fix Eslint issue

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const api = require('./lib/api');
 const log = require('./lib/log');
-const timer = require('./lib/timer');
 
 const DEFAULT_PORT = 1997;
 const DEFAULT_IP = '127.0.0.1';
@@ -13,7 +12,7 @@ log.info(process.env);
 const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || DEFAULT_PORT;
 const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || DEFAULT_IP;
 app.listen(port, ip, log.info('This app is running in port', port));
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Running!!!');
 });
 
