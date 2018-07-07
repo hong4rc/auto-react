acdc7fbbab88af6bba9b44a373df27db70193f63
Hongarc <anhhong12121997t@gmail.com>
2018-07-08 01:42:53 +0700

    Remove some config Heroku

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 'use strict';
-const express = require('express');
-const app = express();
+
 const api = require('./lib/api');
-const log = require('./lib/log');
-
-const DEFAULT_PORT = 1997;
-const DEFAULT_IP = '127.0.0.1';
-
-log.info('process.env');
-log.info(process.env);
-const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || DEFAULT_PORT;
-const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || DEFAULT_IP;
-app.listen(port, ip, log.info('This app is running in port', port));
-app.get('/', (req, res) => {
-    res.send('Running!!!');
-});
 
 api().then(func => {
     func.start();
