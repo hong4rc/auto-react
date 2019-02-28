c5edc6b4dfffb3bfd4304ed639db7429fb4c370b
Ｈｏｎｇａｒｃ <aqua.hong.97@gmail.com>
2019-03-01 02:31:55 +0700

    :bug: duplicated name

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const debug = require('debug');
 
 const BotReact = require('./lib/bot');
 
@@ -7,4 +8,11 @@ const TOKENS = JSON.parse(process.env.TOKENS) || '<Your list token here>';
 TOKENS.forEach((token) => {
   const myBot = new BotReact(token);
   myBot.likeHome();
+});
+
+process.on('SIGINT', () => {
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+  const usedRound = Math.round(used * 100) / 100;
+  debug('memoryUsage')('%d MB', usedRound);
+  process.exit();
 });
